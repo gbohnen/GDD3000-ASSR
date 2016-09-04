d4abcc160a8524d0e06a4647b81837fee672baf4
gbohnen <fezmonkey@live.com>
2016-09-04 14:38:04 -0600

    Starting Game Structure
    - Initial menu structure
    - Basic story backend script added
    - SkyBox assets imported

---
+++ Adventurer Seeks Siberian Refuge/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+    public GameObject buttonPanel;
+    public GameObject creditsPanel;
+    public bool creditsActive = false;
+
+	// click level
+    public void OnClickLevelButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // click credits
+    public void OnClickCreditsButton()
+    {
+        if (!creditsActive)
+        {
+            buttonPanel.GetComponent<CanvasGroup>().alpha = 0;
+            buttonPanel.GetComponent<CanvasGroup>().interactable = false;
+            buttonPanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+            creditsPanel.GetComponent<CanvasGroup>().alpha = 1;
+            creditsPanel.GetComponent<CanvasGroup>().interactable = true;
+            creditsPanel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+            creditsActive = !creditsActive;
+        }
+        else
+        {
+            buttonPanel.GetComponent<CanvasGroup>().alpha = 1;            
+            buttonPanel.GetComponent<CanvasGroup>().interactable = true;
+            buttonPanel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            creditsPanel.GetComponent<CanvasGroup>().alpha = 0;
+            creditsPanel.GetComponent<CanvasGroup>().interactable = false;
+            creditsPanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+            creditsActive = !creditsActive;
+        }        
+    }
+
+    // click exit
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Adventurer Seeks Siberian Refuge/Assets/Scripts/StoryStructure.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StoryStructure : MonoBehaviour {
+
+    // updates the story page. called by buttons and other "progress" keys
+
+    static int totalPages;                  // total pages in current level
+    static int currentPage;                 // currently displayed page
+
+    public GameObject activeText;                  // element that displays the current story
+
+
+    public void UpdateStoryPanel()
+    {
+        // add each dialogue option in a case below. additional cases are easily added. 
+        // use the format described in case 1
+        switch (currentPage)
+        {
+            case 1:
+                activeText.GetComponent<Text>().text = "Put yer strings here.";
+                break;
+            case 2:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+}
